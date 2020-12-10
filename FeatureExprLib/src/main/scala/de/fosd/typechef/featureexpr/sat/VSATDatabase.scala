e38968d395af598dacd761ea50d530e5e6ba26c7
Paul Maximilian Bittner <paul.bittner@uni-ulm.de>
2020-12-10 11:06:12 +0100

    in memory db done #parteeey

--- FeatureExprLib/src/main/scala/de/fosd/typechef/featureexpr/sat/VSATDatabase.scala
+++ FeatureExprLib/src/main/scala/de/fosd/typechef/featureexpr/sat/VSATDatabase.scala
@@ -18,11 +18,21 @@ import slick.jdbc.GetResult
 object VSATDatabase {
     val queriesTableName : String = "Queries";
     val featureModelsTableName : String = "FeatureModels";
+    val noFeatureModel : String = "NoFeatureModel"
 
     val dbDebug : Boolean = false;
 
     var running : Boolean = false;
     var db : Database = null;
+
+    /// TODO:
+    /// Currently, we often wait for futures to complete.
+    /// Instead we should use monadic bind to queue new actions asynchronously.
+    /// I just wanted to get it working first and fancify it afterwards.
+
+    /// TODO:
+    /// There is lots of clone-and-own in the queries.
+    /// We could try to get rid of that but we dont want to win a price so that's low priority.
 
     def init() : Boolean = {
         println("[VSATDatabase.init]");
@@ -72,23 +82,28 @@ object VSATDatabase {
 //            }
 //        }
 
-//        tableExists(queriesTableName).foreach(exists => {
-//            if (!exists) {
-//                runAsync(createQueriesTable())
-//            }
-//        })
-//
-//        tableExists(featureModelsTableName).foreach(exists => {
-//            if (!exists) {
-//                runAsync(createFeatureModelsTable())
-//            }
-//        })
+
+        // Creating tables synchronously and crash if something fails.
         if (!evalForced(tableExists(queriesTableName))) {
             runSyncForced(createQueriesTable())
         }
         if (!evalForced(tableExists(featureModelsTableName))) {
             runSyncForced(createFeatureModelsTable())
         }
+        // Create tables asynchronously and fail silently. This might be a bad idea.
+        //        tableExists(queriesTableName).foreach(exists => {
+        //            if (!exists) {
+        //                runAsync(createQueriesTable())
+        //            }
+        //        })
+        //
+        //        tableExists(featureModelsTableName).foreach(exists => {
+        //            if (!exists) {
+        //                runAsync(createFeatureModelsTable())
+        //            }
+        //        })
+
+//        runSyncForced(printTableNames());
 
         running
     }
@@ -102,52 +117,72 @@ object VSATDatabase {
         running
     }
 
-    /// VSAT logging
 
-    case class SATQueryRecord(formula : String, fmhash : String, mode : String, cacheHits : Int, sentToSAT : Boolean)
-    implicit val getSATQueryRecordResult = GetResult(r => SATQueryRecord(r.<<, r.<<, r.<<, r.<<, r.<<))
+    /// Table Entry Datatypes ///
 
     case class FMRecord(hash : String, formula : String)
     implicit val getFMRecordResult = GetResult(r => FMRecord(r.<<, r.<<))
 
+    // For Slick you dont need an extra class for Primary Keys. Its just for us.
+    case class SATQueryPrimaryKey(formula : String, fmhash : String, mode : String)
+    case class SATQueryRecord(formula : String, fmhash : String, mode : String, tcCacheHits : Int, dbCacheHits : Int, sentToSAT : Boolean)
+    implicit val getSATQueryRecordResult = GetResult(r => SATQueryRecord(r.<<, r.<<, r.<<, r.<<, r.<<, r.<<))
+
+    def fromPrimaryKey(k : SATQueryPrimaryKey, sentToSAT : Boolean) : SATQueryRecord =
+        SATQueryRecord(k.formula, k.fmhash, k.mode, 0, 0, sentToSAT)
+
+
+    /// VSAT logging ///
+
     def cache_hit(the_query: SATFeatureExpr, featureModel: SATFeatureModel): Unit = {
         //println("[Database.cache_hit]")
+        incTcCacheHits(
+            SATQueryPrimaryKey(
+                the_query.toString,
+                featureModel.toString,
+                VSATMissionControl.getCurrentMode().toString));
     }
 
     def record_query(the_query: SATFeatureExpr, featureModel: SATFeatureModel, sentToSat : Boolean): Unit = {
         // We assume that the given query is not yet stored in the DB
         // If it is, we have a cache hit that was missed by TypeChef.
         // This might happen due to repeated runs of TypeChef.
+        // If we get such a cache hit, we store it in dbCacheHits.
 
         // 1.) Store FM if not stored already
-        val fmHash = featureModel.toString
-        if (featureModel != SATNoFeatureModel) {
-        val existingFM : Option[FMRecord] = runSyncForced(sql"SELECT * FROM FeatureModels WHERE hash = $fmHash".as[FMRecord].headOption)
-        if (existingFM.isEmpty) {
-            val fmrecord = FMRecord(fmHash, featureModel.decreate().toString());
-            println("[Database.record_query] inserting new feature model " + fmrecord.hash);
-            runSyncForced(sqlu"INSERT INTO FeatureModels VALUES (${fmrecord.hash}, ${fmrecord.formula});");
+        var fmHash : String = featureModel.toString
+        if (featureModel == SATNoFeatureModel) {
+            //println("[Database.record_query] with SATNoFeatureModel");
+            fmHash = noFeatureModel;
+        } else {
+            val existingFM : Option[FMRecord] = runSyncForced(sql"SELECT * FROM FeatureModels WHERE hash = $fmHash".as[FMRecord].headOption);
+            if (existingFM.isEmpty) {
+                val fmrecord = FMRecord(fmHash, featureModel.decreate().toString());
+                println("[Database.record_query] inserting new feature model " + fmrecord.hash);
+                runSyncForced(sqlu"INSERT INTO FeatureModels VALUES (${fmrecord.hash}, ${fmrecord.formula});");
+            } else {
+                //println("[Database.record_query] fm " + fmHash + " is already stored in db.");
+            }
         }
-}
 
         // 2.) Store the query or update it if we have an accidental cache hit
-//        val mode = VSATMissionControl.getCurrentMode();
-//        val query = SATQueryRecord(the_query.toString, fmHash, mode.toString, 0, sentToSat);
-//
-//        val existingQuery : Option[SATQueryRecord]
-//            = forceEval(sql"SELECT * FROM Queries WHERE formula = ${query.formula} AND fmhash = $fmHash AND mode = ${query.mode}".as[SATQueryRecord].headOption);
-//        if (existingQuery.isEmpty) {
-//            // The default case: This is a new query and we store it
-//            runAsync(sqlu"insert into Queries values (${query.formula}, ${query.fmhash}, ${query.mode}, ${query.cacheHits}, ${query.sentToSAT});")
-//        } else {
-//            // Surprising case: TypeChef told us to record a new query but we actually have a cache hit!
-//            // Such a cache hit remains unobserved by TypeChef.
-//            // Hyothesis: This happens because TypeChef might discard its cache inbetween different runs of the main method.
-//            println("[VSATDatabase.record_query] WE ACTUALLY GOT A CACHE HIT! AND NOW IMPLEMENT A WAY TO HANDLE THAT!")
-//        }
+        val key = SATQueryPrimaryKey(the_query.toString, fmHash, VSATMissionControl.getCurrentMode().toString)
+
+        val existingQuery : Option[SATQueryRecord] = evalForced(getSATQuery(key));
+        if (existingQuery.isEmpty) {
+            // The default case: This is a new query and we store it
+            runAsync(insertSATQuery(fromPrimaryKey(key, sentToSat)));
+        } else {
+            // Surprising case: TypeChef told us to record a new query but we actually have a cache hit!
+            // Such a cache hit remains unobserved by TypeChef.
+            // Hyothesis: This happens because TypeChef might discard its cache inbetween different runs of the main method.
+            //println("[VSATDatabase.record_query] WE ACTUALLY GOT A CACHE HIT! AND NOW IMPLEMENT A WAY TO HANDLE THAT!")
+            incDbCacheHits(key);
+        }
     }
 
-    /// Database utility
+
+    /// Database utility ///
 
     def runAsync[T](action : DBIO[T]) : Future[T] = {
         val f: Future[T] = db.run(action);
@@ -170,7 +205,8 @@ object VSATDatabase {
     // like eval but throws an exception when the future fails
     def evalForced[T](f : Future[T]) : T = Await.result(f, Duration.Inf)
 
-    // database logic
+
+    /// SQL Queries ///
 
     /// sqlu produces a DBIO[Int] action where the returned int is the row cound (I guess)
     def createQueriesTable() : DBIO[Int] =
@@ -178,20 +214,11 @@ object VSATDatabase {
                formula varchar(max) NOT NULL,
                fmhash varchar(255) NOT NULL,
                mode varchar(50) NOT NULL,
-               cacheHits int NOT NULL,
+               tcCacheHits int NOT NULL,
+               dbCacheHits int NOT NULL,
                sentToSAT bool NOT NULL,
                CONSTRAINT pkey PRIMARY KEY(formula, fmhash, mode)
                );"""
-
-    def showQueriesTable() : DBIO[Unit] = {
-        val sep = " | ";
-        sql"SELECT * FROM Queries".as[SATQueryRecord].map { satqueries =>
-            println("[VSATDatabase.showQueriesTable] SAT_QUERIES:")
-            println("  formula"+sep+"fmhash"+sep+"mode"+sep+"cacheHits"+sep+"sentToSAT")
-            for(q <- satqueries)
-                println("  " + q.formula.substring(0, 20) + "..." + sep + q.fmhash + sep + q.mode + sep + q.cacheHits + sep + q.sentToSAT)
-        }
-    }
 
     def createFeatureModelsTable() : DBIO[Int] =
         sqlu"""CREATE TABLE FeatureModels (
@@ -199,16 +226,6 @@ object VSATDatabase {
                formula varchar(max) NOT NULL,
                PRIMARY KEY(hash)
                );"""
-
-    def showFeatureModelsTable() : DBIO[Unit] = {
-        val sep = " | ";
-        sql"SELECT * FROM FeatureModels".as[FMRecord].map { fms =>
-            println("[VSATDatabase.showFeatureModelsTable] FEATURE_MODELS:")
-            println("  hash"+sep+"formula")
-            for(fm <- fms)
-                println("  " + fm.hash + sep + fm.formula.substring(0, 20))
-        }
-    }
 
     def tableExists(tablename : String) : Future[Boolean] = {
         runAsync(sql"""SELECT TABLE_NAME
@@ -218,7 +235,91 @@ object VSATDatabase {
         ).map(o => o.nonEmpty)
     }
 
-    /// Test methods
+    def getSATQuery(key: SATQueryPrimaryKey) : Future[Option[SATQueryRecord]] =
+        runAsync(sql"SELECT * FROM Queries WHERE formula = ${key.formula} AND fmhash = ${key.fmhash} AND mode = ${key.mode}".as[SATQueryRecord].headOption)
+
+    def insertSATQuery(q : SATQueryRecord) : DBIO[Int] =
+        sqlu"insert into Queries values (${q.formula}, ${q.fmhash}, ${q.mode}, ${q.tcCacheHits}, ${q.dbCacheHits}, ${q.sentToSAT});"
+
+    def incTcCacheHits(key: SATQueryPrimaryKey) : Future[Int] = {
+        getSATQuery(key).flatMap {
+            case Some(satQuery) => {
+                val newTcCacheHits = 1 + satQuery.tcCacheHits;
+                runAsync(
+                    sqlu"""UPDATE Queries
+                          SET tcCacheHits=$newTcCacheHits
+                          WHERE formula = ${satQuery.formula}
+                          AND fmhash = ${satQuery.fmhash}
+                          AND mode = ${satQuery.mode};""")
+            };
+            case None => throw new Exception("[VSATDatabase.incTcCacheHits] The given query is not stored in the database yet!");
+        }
+    }
+
+    // #clone-and-own
+    def incDbCacheHits(key: SATQueryPrimaryKey) : Future[Int] = {
+        getSATQuery(key).flatMap {
+            case Some(satQuery) => {
+                val newDbCacheHits = 1 + satQuery.dbCacheHits;
+                runAsync(
+                    sqlu"""UPDATE Queries
+                          SET dbCacheHits=$newDbCacheHits
+                          WHERE formula = ${satQuery.formula}
+                          AND fmhash = ${satQuery.fmhash}
+                          AND mode = ${satQuery.mode};""")
+            };
+            case None => throw new Exception("[VSATDatabase.incDbCacheHits] The given query is not stored in the database yet!");
+        }
+    }
+
+
+    /// Debug SQL Queries ///
+
+    def showQueriesTable() : DBIO[Unit] = {
+        val sep = " | ";
+        sql"SELECT * FROM Queries".as[SATQueryRecord].map { satqueries =>
+            println("[VSATDatabase.showQueriesTable] SAT_QUERIES:")
+            println("  formula"+sep+"fmhash"+sep+"mode"+sep+"cacheHits"+sep+"sentToSAT")
+            for(q <- satqueries)
+                println("  " + longStringPreview(q.formula) + sep + q.fmhash + sep + q.mode + sep + q.tcCacheHits + sep + q.dbCacheHits + sep + q.sentToSAT)
+        }
+    }
+
+    def showFeatureModelsTable() : DBIO[Unit] = {
+        val sep = " | ";
+        sql"SELECT * FROM FeatureModels".as[FMRecord].map { fms =>
+            println("[VSATDatabase.showFeatureModelsTable] FEATURE_MODELS:")
+            println("  hash"+sep+"formula")
+            for(fm <- fms)
+                println("  " + fm.hash + sep + longStringPreview(fm.formula))
+        }
+    }
+
+    def printTableNames() : DBIO[Unit] = {
+        sql"""SELECT TABLE_NAME
+               FROM INFORMATION_SCHEMA.TABLES
+               WHERE TABLE_SCHEMA = 'PUBLIC'""".as[String].map {
+            names =>
+                println("[VSATDatabase.printTableNames]")
+                for(t <- names)
+                    println("  " + t)
+        }
+    }
+
+
+    /// Util ///
+
+    def longStringPreview(s : String) : String = {
+        val limit = 20;
+        if (s.length() > limit) {
+            s.substring(0, limit-3) + "..."
+        } else {
+            s
+        }
+    }
+
+
+    /// Test SQL Queries ///
 
     def createTestTable() : DBIO[Unit] = {
         DBIO.seq(sqlu"CREATE TABLE TEST (id int NOT NULL, name varchar(255) NOT NULL);")
